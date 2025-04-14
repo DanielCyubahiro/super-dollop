e0e1aebb61b162733df31722218fc10bca2ce4c0
Daniel <cyubahirodaniel@gmail.com>
2025-04-14 17:32:03 +0200

    setups

--- styles.js
+++ styles.js
@@ -1,14 +1,16 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-  *,
-  *::before,
-  *::after {
-    box-sizing: border-box;
-  }
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
 
-  body {
-    margin: 0;
-    font-family: system-ui;
-  }
+    body {
+        margin: 1rem;
+        font-family: system-ui;
+    }
 `;
