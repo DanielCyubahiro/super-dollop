e35ad29712a826d03cc454f29a9001882f233791
Daniel <cyubahirodaniel@gmail.com>
2025-04-14 17:31:49 +0200

    Add list item component to render all the images

---
+++ components/ListItem/ListItem.styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const ListItemContainerStyled = styled.div`
+    background: hsl(0 0% 100%);
+    border-radius: 0.5rem;
+    border: 1px solid  hsl(240 3.7% 15.9% / 0.1);
+    box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
+    padding: 1rem;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const StyledImage = styled(Image)`
+border-radius: 2rem;
+`
