226cbec342064132575d8c73c227885d92935a97
Daniel <cyubahirodaniel@gmail.com>
2025-04-14 17:31:27 +0200

    Add list component

---
+++ components/List/List.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledMessage = styled.p`
+    font-size: 2rem;
+    text-align: center;
+`
+
+export const ListStyled = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+`
