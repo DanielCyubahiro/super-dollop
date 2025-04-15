839b34097731084b265a13b342041d9df7da99da
Daniel <cyubahirodaniel@gmail.com>
2025-04-15 15:16:07 +0200

    Add data persistance with localstorage

--- stores/artPiecesStore.js
+++ stores/artPiecesStore.js
@@ -1,31 +1,43 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useArtPiecesStore = create((set, get) => ({
-  artPieces: [],
-  isLoading: false,
-  error: null,
-  fetchArtPieces: async () => {
-    set({ isLoading: true });
-    try {
-      const response = await fetch("https://example-apis.vercel.app/api/art");
-      if (!response.ok) {
-        throw new Error("Error fetching art pieces");
-      }
-      const data = await response.json();
-      set({
-        artPieces: data.map(piece => ({ ...piece, isFavorite: false })),
-        isLoading: false,
-      });
-    } catch (error) {
-      set({ error, isLoading: false });
-    }
-  },
-  toggleFavorite: (slug) => {
-    set(state => ({
-      artPieces: state.artPieces.map(piece => piece.slug === slug
-        ? { ...piece, isFavorite: !piece.isFavorite }
-        : piece,
-      ),
-    }));
-  },
-}));
+export const useArtPiecesStore = create(
+  persist(
+    (set, get) => ({
+      artPieces: [],
+      isLoading: false,
+      error: null,
+      fetchArtPieces: async () => {
+        // Only fetch if we don't have data
+        if (get().artPieces.length === 0 && !get().isLoading) {
+          set({ isLoading: true });
+          try {
+            const response = await fetch(
+              "https://example-apis.vercel.app/api/art");
+            if (!response.ok) {
+              throw new Error("Error fetching art pieces");
+            }
+            const data = await response.json();
+            set({
+              artPieces: data.map(piece => ({ ...piece, isFavorite: false })),
+              isLoading: false,
+            });
+          } catch (error) {
+            set({ error, isLoading: false });
+          }
+        }
+      },
+      toggleFavorite: (slug) => {
+        set(state => ({
+          artPieces: state.artPieces.map(piece => piece.slug === slug
+            ? { ...piece, isFavorite: !piece.isFavorite }
+            : piece,
+          ),
+        }));
+      },
+    }),
+    {
+      name: "art-pieces-storage",
+    },
+  ),
+);
