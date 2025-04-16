fbe755c8e51bbe43632b74328e75fed5a05a6bc3
Daniel <cyubahirodaniel@gmail.com>
2025-04-16 17:56:45 +0200

    Update spotlight

--- components/Spotlight/Spotlight.styled.js
+++ components/Spotlight/Spotlight.styled.js
@@ -11,23 +11,4 @@ export const StyledSpotlightBackground = styled.div`
     width: 100%;
     height: 100%;
     z-index: -1;
-
-    &::before {
-        content: attr(data-before-text);
-        font-size: 1rem;
-        font-style: italic;
-        color: white;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.5);
-    }
-`
-
-export const StyledSpotlightFavButtonContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    min-height: 95vh;
 `
--- pages/spotlight/index.js
+++ pages/spotlight/index.js
@@ -1,34 +1,14 @@
 import useRandomArtPiece from "@/hooks/useRandomArtPiece";
 import Spotlight from "@/components/Spotlight/Spotlight";
-import FavoriteButton from "@/components/Buttons/FavoriteButton/FavoriteButton";
-import { Heart } from "lucide-react";
-import {
-  StyledSpotlightFavButtonContainer,
-} from "@/components/Spotlight/Spotlight.styled";
 
 const SpotlightPage = () => {
   const artToSpotlight = useRandomArtPiece();
 
   return artToSpotlight && (
-    <>
-      <Spotlight
-        backgroundImage={artToSpotlight.imageSource}
-        artistName={artToSpotlight.artist}
-      />
-      <StyledSpotlightFavButtonContainer>
-        <FavoriteButton
-          data-testid="spotlight-favorite-button"
-          slug={artToSpotlight.slug}
-          transparent
-        >
-          {artToSpotlight.isFavorite ? (
-            <Heart size={"18"} fill={"white"} />
-          ) : (
-            <Heart size={"18"} />
-          )}
-        </FavoriteButton>
-      </StyledSpotlightFavButtonContainer>
-    </>
+    <Spotlight
+      backgroundImage={artToSpotlight.imageSource}
+      artistName={artToSpotlight.artist}
+    />
   )
     ;
 };
